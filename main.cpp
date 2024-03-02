fe0dacc4e05ea415bd73836b1ee887857827c00e
NgoTheTan <23021691@vnu.edu.vn>
2024-03-02 08:48:31 +0700

    BT_STRUCT
    Bai tap struct p1 p2

--- main.cpp
+++ main.cpp
@@ -1,157 +1,96 @@
-#include <iostream>
-#include <SDL.h>
-#include <SDL_image.h>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-const char* WINDOW_TITLE = "Hello World!";
-
-void logErrorAndExit(const char* msg, const char* error)
+struct Point
 {
-    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s: %s", msg, error);
-    SDL_Quit();
-}
-
-SDL_Window* initSDL(int SCREEN_WIDTH, int SCREEN_HEIGHT, const char* WINDOW_TITLE)
-{
-    if (!IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG))
-        logErrorAndExit( "SDL_image error:", IMG_GetError());
-
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-        logErrorAndExit("SDL_Init", SDL_GetError());
-
-    SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    //full screen
-    //window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
-    if (window == nullptr) logErrorAndExit("CreateWindow", SDL_GetError());
-
-    return window;
-}
-SDL_Texture *loadTexture(const char *filename, SDL_Renderer* renderer)
-{
-	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO,
-                     "Loading %s", filename);
-
-	SDL_Texture *texture = IMG_LoadTexture(renderer, filename);
-	if (texture == NULL) {
-        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR,
-                       "Load texture %s", IMG_GetError());
-      }
-
-	return texture;
-}
-SDL_Renderer* createRenderer(SDL_Window* window)
-{
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
-                                              SDL_RENDERER_PRESENTVSYNC);
-    //Khi chạy trong máy ảo (ví dụ phòng máy ở trường)
-    //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
-
-    if (renderer == nullptr) logErrorAndExit("CreateRenderer", SDL_GetError());
-
-    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
-    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    return renderer;
-}
-void renderTexture(SDL_Texture *texture, int x, int y,
-                   SDL_Renderer* renderer)
-{
-	SDL_Rect dest;
-	dest.x = x;
-	dest.y = y;
-	dest.w=300;
-	dest.h=200;
-
-	SDL_RenderCopy(renderer, texture, NULL, &dest);
-}
-
-
-void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
-{
-    IMG_Quit();
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-}
-
-void waitUntilKeyPressed()
-{
-    SDL_Event e;
-    while (true) {
-        if ( SDL_PollEvent(&e) != 0 &&
-             (e.type == SDL_KEYDOWN || e.type == SDL_QUIT) )
-            return;
-        SDL_Delay(100);
+    float x, y;
+    Point()
+    {
+        x=0; y=0;
     }
-}
+    Point(float _x, float _y)
+    {
+        x=_x; y=_y;
+    }
+};
 
-void drawSomething(SDL_Window* window, SDL_Renderer* renderer) {
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);   // white
-    SDL_RenderDrawPoint(renderer, SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 100);   // red
-    SDL_RenderDrawLine(renderer, 100, 50, 200, 200);
-    SDL_Rect filled_rect;
-    filled_rect.x = SCREEN_WIDTH - 400;
-    filled_rect.y = SCREEN_HEIGHT - 150;
-    filled_rect.w = 320;
-    filled_rect.h = 100;
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // green
-    SDL_RenderFillRect(renderer, &filled_rect);
-    SDL_Rect filled_rect2;
-    filled_rect2.x=200;
-    filled_rect2.y=100;
-    filled_rect2.w=100;
-    filled_rect2.h=150;
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); //blue
-    SDL_RenderFillRect(renderer, &filled_rect2);
-}
-
-int main(int argc, char* argv[])
+void passByValue (Point a)
 {
-    //Khởi tạo môi trường đồ họa
-    SDL_Window* window = initSDL(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
-    SDL_Renderer* renderer = createRenderer(window);
-
-    /*//Xóa màn hình
-    SDL_RenderClear(renderer);
-
-    //Vẽ gì đó
-    drawSomething(window, renderer);*/
-    SDL_Texture* background=loadTexture("image\\test.jpg",renderer);
-    SDL_RenderCopy(renderer, background, NULL, NULL);
-    SDL_RenderPresent(renderer);
-    SDL_DestroyTexture(background);
-    background=NULL;
-    waitUntilKeyPressed();
-
-    SDL_Texture* spongeBob = loadTexture("image\\Spongebob.png", renderer);
-    renderTexture(spongeBob, 500, 300, renderer);
-    SDL_RenderPresent(renderer);
-
-    waitUntilKeyPressed();
-
-    SDL_Rect spongeBobSrc;
-    spongeBobSrc.x=0;
-    spongeBobSrc.y=0;
-    spongeBobSrc.h=150;
-    spongeBobSrc.w=150;
-    SDL_RenderCopy(renderer, spongeBob, &spongeBobSrc, NULL);
-
-    //Hiện bản vẽ ra màn hình
-    //Khi chạy tại môi trường bình thường
-    SDL_RenderPresent(renderer);
-    //Khi chạy trong máy ảo (ví dụ phòng máy ở trường)
-    //SDL_UpdateWindowSurface(window);
-
-
-    //Đợi phím bất kỳ trước khi đóng môi trường đồ họa và kết thúc chương trình
-    waitUntilKeyPressed();
-    SDL_DestroyTexture(spongeBob);
-    spongeBob=NULL;
-
-    quitSDL(window, renderer);
-    return 0;
+    cout << &a << endl;
+    float temp=a.x;
+    a.x=a.y;
+    a.y=temp;
 }
+void passByReference(Point &a)
+{
+    cout << &a << endl;
+    float temp=a.x;
+    a.x=a.y;
+    a.y=temp;
+}
+ void print(Point p)
+ {
+     cout << p.x << " " << p.y << endl;
+ }
+ Point midPoint(const Point a,const Point b)
+ {
+     Point m;
+     m.x=(a.x+b.x)/2;
+     m.y=(a.y+b.y)/2;
+     return m;
+ }
+
+ struct Rect
+ {
+     float x, y, w, h;
+     Rect()
+     {
+         x=0; y=0; w=0; h=0;
+     }
+     Rect(float _x, float _y, float _w, float _h)
+     {
+         x=_x; y=_y; w=_w; h=_h;
+     }
+     bool contains(const Point p)
+     {
+         if (p.x>=x && p.y<=y && p.x<=x+w && p.y>=y-h) return true;
+         return false;
+     }
+ };
+
+ struct Ship
+ {
+     Rect board; string id; float dx; float dy;
+     Ship(){}
+     Ship(Rect _board, string _id, float _dx, float _dy)
+     {
+         board=_board; id=_id; dx=_dx; dy=_dy;
+     }
+     void move()
+     {
+         board.x+=dx; board.y+=dy;
+     }
+ };
+
+ void display(const Ship &ship)
+ {
+     cout << ship.id << endl << ship.board.x << " " << ship.board.y << endl;
+ }
+ //Cau8: vi tri tuong doi cua mot bien kieu Point va truong x cua no la trung nhau, trong khi truong y nam ben canh ve phia ben phai truong x.
+
+ int main()
+ {
+     //demo struct Point, Rect va Ship
+     Point a(1.1,1.8), b(3.3,4.4);
+     print(midPoint(a,b)); // in ra 2.2 3.3
+     Rect hcn(1, 2, 3, 4);
+     if (hcn.contains(a)) cout << "yes" << endl;
+     else cout << "no" << endl; // in ra yes
+     if (hcn.contains(b)) cout << "yes" << endl;
+     else cout << "no" << endl; //in ra no
+     Ship Tan(hcn, "TAN", 1, 2);
+     for (int i=0; i<5; i++){
+        Tan.move();
+        display(Tan);
+     }
+     return 0;
+ }
