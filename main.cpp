aa295d5b1ed17043a385f29ad6d560603e68a599
NgoTheTan <23021691@vnu.edu.vn>
2024-03-26 17:33:11 +0700

    BT Ham

--- main.cpp
+++ main.cpp
@@ -1,295 +1,201 @@
 #include <bits/stdc++.h>
 #include <cstdlib>
 #include <ctime>
-
+#include <cmath>
 using namespace std;
 
-/*Cau 1:
-    a.Khi khai bao mang ngoai ham main, cac gia tri cua mang duoc gan voi gia tri mac dinh la 0. Nguoc lai trong ham main thi gia tri la cac so sinh ngau nhien.
-    b.Khi N>4, cac gia tri khac cua mang deu bang 0 du khai bao trong hay ngoai ham main. Khi N<4 se bi loi tran mang va run-time vi so luong phan tu trong day khai bao nhieu hon
-    so kich thuoc khai bao cua mang.
-    c. Khi khai bao theo dang a[]={1,2,3,4} thi du o trong hay ngoai ham main, mang chi co 4 phan tu la 1 2 3 4.
-  Cau 2:
-    a.Neu khong khoi tao gia tri thi trong hay ngoai ham main deu gap loi bien dich vi thieu kich thuoc cua mang a.
-    b. char a[N]="abcd" neu N>4 thi khong co loi, nho hon hoac bang 4 thi co loi tran kich thuoc mang. Gia tri cua mang van la {'a','b','c','d','/0'} du trong hay ngoai main;
-    c. Kich thuoc mang thuc la 5. tim bang phep tinh sizeof(a)/sizeof(char);
-    => Mang co kich thuoc N luu duoc xau co do dai toi da N-1;
-  Cau 3:
-    a. Hieu qua khoi tao la nhu nhau
-    b. Voi cach khoi tao chia thanh 2 mang con thi khi xoa bot phan tu, phan tu khong nhan gia tri tai moi mang se nhan gia tri bang 0. Con voi cach khai bao gop lai thanh 1 thi
-    khi xoa bot phan tu, cac phan tu co gia tri bang 0 nam o cuoi dong 2 (tuc o vi tri cuoi cung cua mang).
-    c. Chi co the bo bot so cot 12 di.
-  Cau 4:
-    a. Khong bi loi (vi C++ khong check tran mang). Cac gia tri in ra la ngau nhien (khac gia tri khoi tao la 0) nen biet duoc rang dang truy cap ngoai mang, chi duy co truong hop
-    chi so <0 nhu -1 -2 -3 thi in ra gia tri bang 0.
-    c. Mang nhieu chieu co dinh so phan tu dung bang gia tri da khai bao, khong bi tran mang.
-  Cau 5:
+/* Phan A:
+    Cau 1:
+    - Dia chi cua cac phan tu trong mang se tang dan, hon kem nhau dung bang so byte dung de luu tru kieu do, vi du int la 4 byte.
+    - Cac bien duoc khai bao truoc, sau hoac giua hai mang tren co vi tri tuong doi cach nhau tuy thuoc vao so phan tu trong mang (tat nhien theo so byte luu tru cua kieu du lieu)
+    Cau 2:
+    Cac chuoi gia tri dia chi duoc in ra cach deu nhau mot khoang 40 (theo he hexa). Kich thuoc cua mot stack frame cho ham factorial la khoang 64 byte khi dung kieu long long va 48 byte
+    khi dung kieu int.
+    Cau 3:
+    void passByValue(int n)
+    {
+    cout << &n << endl;
+    }
+    void passByReference(int &n)
+    {
+    cout << &n << endl;
+    }
+    int main()
+    {
+    int n; cin >> n;
+    cout << &n << endl;
+    passByReference(n); passByValue(n);
+    return 0;
+    }
+    output cho ra 0x61fe1c 0x61fe1c 0x61fdf0
+    Cau 4:
+    a, Mang duoc truyen vao ham theo co che pass-by-reference
+    b, C++ string duoc truyen vao ham theo co che pass-by-value
+    Cau 5:
+    int main()
+    {
+        int a=4, c=5;
+        int &b=a;
+        cout << &a << " "<< &b << endl;
+        b=c;
+        cout << &c << " " << &b << endl;
+    }
+    a, Bien tham chieu va bien ma no chieu toi la cung mot bien trong bo nho.
+    b, Khong the khai bao mot bien tham chieu ma chua chieu ngay no toi mot bien thuong.
+    c, Khong the chieu mot tham chieu toi mot bien khac voi dich ban dau cua no, tuc nhu o doan code tren, b van chieu toi a sau khi da gan voi c.
+    Cau 6:
+    Khi chay xong ham f, ham f duoc giai phong khoi bo nho va chay den ham g, khi do ham g lai the cho vao bo nho cua ham f (nhat la khi hai ham cung kieu void, tham so
+    giong nhau cung dang int) nen ket qua dua ra 2 dia chi giong nhau.
 
 */
-
-string findDuplicate(const int a[],const int n)
+bool majority(bool a, bool b, bool c)
 {
-    bool seen[n]={};
-    for (int i=0; i<n; i++){
-        if (seen[a[i]-1]) return "Yes";
-        seen[a[i]-1]=true;
-    }
-    return "No";
+    return a&&b || b&&c || a&&c;
 }
-string xauDoiXung(const char a[], const int len)
+bool eq(int a[], int size_a, int b[], int size_b, int n)
 {
-    int i=0, j=len-1;
-    while (i<j){
-        if (a[i]!=a[j]) return "No";
-        i++; j--;
-    }
-    return "Yes";
-}
-bool checkSoDoiGuong(const int n)
-{
-    int temp1=n, temp2=n, reversedN=0, cnt=0;
-    while (temp1!=0){
-        cnt++;
-        temp1/=10;
-    }
-    while (temp2!=0){
-        int digit=temp2%10;
-        if (digit!=0){
-            for (int i=0; i<cnt-1; i++){
-                digit*=10;
-            }
-            reversedN+=digit;
+    int cnt=0, i=0, j=0;
+    sort(a, a+size_a); sort(b, b+size_b);
+    while (i<size_a && j <size_b){
+        if (a[i]==b[j]){
+            cnt++;
+            if (cnt==n) return true;
+            i++; j++;
         }
-        cnt--;
-        temp2/=10;
+        else{
+            cnt=0; i++;
+        }
     }
-    if (reversedN==n) return true;
     return false;
 }
-int soDoiGuong(const int a,const int b)
+bool checkPrime(int n)
 {
-    int cnt=0;
-    for (int i=a; i<=b; i++){
-        if (checkSoDoiGuong(i)) cnt++;
+    for (int i=0; i<=sqrt(n); i++){
+        if (n%i==0)return false;
     }
-    return cnt;
+    return n>1;
 }
-void mineSwepper(char a[][10], const int m, const int n)
+void swap(int *a, int *b)
 {
-    if (a[0][0]!='*'){
-        int cnt=0;
-        if (a[0][1]=='*') cnt++;
-        if (a[1][0]=='*') cnt++;
-        if (a[1][1]=='*') cnt++;
-        a[0][0]=cnt+'0';
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+int gcd(int a, int b)
+{
+    if (a<b){
+        swap(a,b);
     }
-    if (a[m-1][0]!='*'){
-        int cnt=0;
-        if (a[m-2][0]=='*') cnt++;
-        if (a[m-1][1]=='*') cnt++;
-        if (a[m-2][1]=='*') cnt++;
-        a[m-1][0]=cnt+'0';
+    int r=a%b;
+    while (r!=0){
+        a=b;
+        b=r;
+        r=a%b;
     }
-    if (a[0][n-1]!='*'){
-        int cnt=0;
-        if (a[0][n-2]=='*') cnt++;
-        if (a[1][n-1]=='*') cnt++;
-        if (a[1][n-2]=='*') cnt++;
-        a[0][n-1]=cnt+'0';
-    }
-    if (a[m-1][n-1]!='*'){
-        int cnt=0;
-        if (a[m-1][n-2]=='*') cnt++;
-        if (a[m-2][n-1]=='*') cnt++;
-        if (a[m-2][n-2]=='*') cnt++;
-        a[m-1][n-1]=cnt+'0';
-    }
-    for (int i=1; i<m-1; i++){
-        if (a[i][0]!='*'){
-            int cnt=0;
-            if (a[i-1][0]=='*') cnt++;
-            if (a[i-1][1]=='*') cnt++;
-            if (a[i][1]=='*') cnt++;
-            if (a[i+1][0]=='*') cnt++;
-            if (a[i+1][1]=='*') cnt++;
-            a[i][0]=cnt+'0';
-        }
-    }
-    for (int i=1; i<m-1; i++){
-        if (a[i][n-1]!='*'){
-            int cnt=0;
-            if (a[i-1][n-1]=='*') cnt++;
-            if (a[i-1][n-2]=='*') cnt++;
-            if (a[i][n-2]=='*') cnt++;
-            if (a[i+1][n-1]=='*') cnt++;
-            if (a[i+1][n-2]=='*') cnt++;
-            a[i][n-1]=cnt+'0';
-        }
-    }
-    for (int i=1; i<n-1; i++){
-        if (a[0][i]!='*'){
-            int cnt=0;
-            if (a[0][i+1]=='*') cnt++;
-            if (a[0][i-1]=='*') cnt++;
-            if (a[1][i]=='*') cnt++;
-            if (a[1][i-1]=='*') cnt++;
-            if (a[1][i+1]=='*') cnt++;
-            a[0][i]=cnt+'0';
-        }
-    }
-    for (int i=1; i<n-1; i++){
-        if (a[m-1][i]!='*'){
-            int cnt=0;
-            if (a[m-1][i+1]=='*') cnt++;
-            if (a[m-1][i-1]=='*') cnt++;
-            if (a[m-2][i]=='*') cnt++;
-            if (a[m-2][i-1]=='*') cnt++;
-            if (a[m-2][i+1]=='*') cnt++;
-            a[m-1][i]=cnt+'0';
-        }
-    }
-    for (int i=1; i<m-1; i++){
-        for (int j=0; j<n-1; j++){
-            if (a[i][j]!='*'){
-                int cnt=0;
-                if (a[i-1][j-1]=='*') cnt++;
-                if (a[i-1][j]=='*') cnt++;
-                if (a[i-1][j+1]=='*') cnt++;
-                if (a[i][j-1]=='*') cnt++;
-                if (a[i][j+1]=='*') cnt++;
-                if (a[i+1][j-1]=='*') cnt++;
-                if (a[i+1][j]=='*') cnt++;
-                if (a[i+1][j+1]=='*') cnt++;
-                a[i][j]=cnt+'0';
-            }
-        }
-    }
+    return b;
+}
+void printLine(int m, int n)
+{
     for (int i=0; i<m; i++){
-        for (int j=0; j<n; j++){
-            cout << a[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-void snailArray(const int m, const int n)
-{
-    int a[m][n]={{},{}};
-    int maximum=m*n, k=1;
-    int chieu_dai=n, chieu_rong=m, i=0, j=0, rounds1=0, rounds2=1;
-    while (k<=maximum){
-        while (j<chieu_dai){
-            a[i][j]=k;
-            k++;
-            j++;
-        }
-        if (k>maximum) break;
-        j--; k--;
-        while (i<chieu_rong){
-            a[i][j]=k;
-            k++;
-            i++;
-        }
-        if (k>maximum) break;
-        i--; k--;
-        while (j>=rounds1){
-            a[i][j]=k;
-            k++;
-            j--;
-        }
-        if (k>maximum) break;
-        j++; k--;
-        while (i>=rounds2){
-            a[i][j]=k;
-            k++;
-            i--;
-        }
-        if (k>maximum) break;
-        i++; k--;
-        chieu_dai--; chieu_rong--; rounds1++; rounds2++;
-    }
-    for (int i=0; i<m; i++){
-        for (int j=0; j<n; j++){
-            cout << a[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-void magicalArray(const int n)
-{
-    int count=1;
-    int a[n][n];
-    for (int i=0; i<n; i++){
-        for (int j=0; j<n; j++){
-            a[i][j]=0;
-        }
-    }
-    a[0][n/2]=1;
-    int x=0;
-    int y=n/2;
-
-    for(int i=0; i<n*n; i++){
-        if(x==0 and (y!=n-1) and a[n-1][y+1]==0){
-            x=n-1;
-            y++;
-            count++;
-            a[x][y]=count;
-        } else if(x!=0 and y!=n-1 and a[x-1][y+1]==0){
-            x=x-1;
-            y++;
-            count++;
-            a[x][y]=count;
-        } else if(x!=0 and y==n-1 and a[x-1][0]==0){
-            x=x-1;
-            y=0;
-            count++;
-            a[x][y]=count;
-        } else if(x!=0 and y!=n-1 and a[x-1][y+1]!=0){
-            x=x+1;
-            count++;
-            a[x][y]=count;
-        } else if(x==0 and y==n-1){
-            x=x+1;
-            count++;
-            a[x][y]=count;
-        }
-    }
-
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cout << a[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-int main()
- {
-    /* cau 6 phan a
-    int n=30;
-    srand(time(NULL));
-    int a[n];
-    for (int i=0; i<n; i++){
-        a[i]=rand()%10;
+        cout << " ";
     }
     for (int i=0; i<n; i++){
-        cout << a[i] << " ";
+        cout << "*";
     }
     cout << endl;
-    for (int k = n - 1; k > 0; k--){
-        bool swapped = false;
-        for (int i = 1; i <= k; i++){
-            if (a[i-1] > a[i]){
-                int temp = a[i];
-                a[i] = a[i-1];
-                a[i-1] =  temp;
-                swapped = true;
+}
+void printTriangle(int lines)
+{
+    for (int i=0; i<lines; i++){
+        printLine(lines-i-1, 2*i+1);
+    }
+}
+int function_rnd(double x)
+{
+    int res;
+    if (x+0.5 < ceil(x))
+    {
+        res=floor(x);
+    }
+    else{
+        res=ceil(x);
+    }
+    return res;
+}
+int basic_rnd(double x)
+{
+    int temp=(int)x;
+    if (x-(double)temp >= 0.5) return temp+1;
+    return temp;
+}
+int lessThan(int n)
+{
+    return rand()%n;
+}
+void threeDivide25(int a[], int n)
+{
+    for (int i=0; i<n; i++){
+        for (int j=i+1; j<n; j++){
+            for (int k=j+1; k<n; k++){
+                if ((a[i]+a[j]+a[k])%25==0) cout << a[i] << " " << a[j] << " " << a[k] << endl;
             }
         }
-        if (!swapped) break;
     }
-    for (int i=0; i<n; i++){
-        cout << a[i] << " ";
-    }*/
-    int n; cin >> n;
-    magicalArray(n);
+}
+string tenToTwo(int n)
+{
+    string binary="";
+    while (n!=0){
+        if (n%2==0) binary+='0';
+        else binary+='1';
+        n/=2;
+    }
+    reverse(binary.begin(), binary.end());
+    return binary;
+}
+int twoToTen(string binary)
+{
+    int decimal=0;
+    int len=binary.length();
+    reverse(binary.begin(), binary.end());
+    for (int i=0; i<len; i++){
+        if (binary[i]=='1') decimal+=pow(2,i);
+    }
+    return decimal;
+}
+int factorial(int n)
+{
+    int res=1;
+    for (int i=1; i<=n; i++){
+        res*=i;
+    }
+    return res;
+}
+int toHop(int k, int n)
+{
+    return factorial(n)/(factorial(k)*(factorial(n-k)));
+}
+int kiemtra(int k, int n)
+{
+    if (k>=0 && k<=n && n>=1 && n<=20) return 1;
     return 0;
- }
+}
+//void nhapKN(int k[], int n[], int *soPhanTu)
+//{
+//    *soPhanTu=0;
+//    while (true){
+//        cin >> k[*soPhanTu] >> n[*soPhanTu];
+//        if (k[*soPhanTu]==-1 || n[*soPhanTu]==-1) break;
+//        soPhanTu++;
+//    }
+//}
+int main()
+{
+    srand(time(NULL));
+    int n; cin >> n;
+    int a[n];
+    for (int i=0; i<n; i++){
+        a[i]=rand()%50;
+    }
+    threeDivide25(a,n);
+}
