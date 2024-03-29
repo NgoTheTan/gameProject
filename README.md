41ec7dc94965aa26145f56e50e4ed1c4a6f39664
NgoTheTan <23021691@vnu.edu.vn>
2024-03-29 10:39:14 +0700

    BT Pointer

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # gameProject
- making a simple game
+
+B/Cau hoi:
+Cau 1: Output la
+*p3 = B, p3 = 0x61fe06
+*p3 = A, p3 = 0x61fe07
+*p1 = B, p1 = 0x61fe07
+(da phai them (void*) vao cac bien de in ra dia chi)
+Cau 2: Cau lenh gan cho i gia tri 5 la *p=5
+Cau 3: Loi o day la sai kieu du lieu. Bien c kieu char thi con tro p den c cung phai co kieu char, o day con tro lai co kieu double se cho ra ket qua sai.
--- main.cpp
+++ main.cpp
@@ -3,199 +3,55 @@
 #include <ctime>
 #include <cmath>
 using namespace std;
-
-/* Phan A:
-    Cau 1:
-    - Dia chi cua cac phan tu trong mang se tang dan, hon kem nhau dung bang so byte dung de luu tru kieu do, vi du int la 4 byte.
-    - Cac bien duoc khai bao truoc, sau hoac giua hai mang tren co vi tri tuong doi cach nhau tuy thuoc vao so phan tu trong mang (tat nhien theo so byte luu tru cua kieu du lieu)
-    Cau 2:
-    Cac chuoi gia tri dia chi duoc in ra cach deu nhau mot khoang 40 (theo he hexa). Kich thuoc cua mot stack frame cho ham factorial la khoang 64 byte khi dung kieu long long va 48 byte
-    khi dung kieu int.
-    Cau 3:
-    void passByValue(int n)
-    {
-    cout << &n << endl;
-    }
-    void passByReference(int &n)
-    {
-    cout << &n << endl;
-    }
-    int main()
-    {
-    int n; cin >> n;
-    cout << &n << endl;
-    passByReference(n); passByValue(n);
-    return 0;
-    }
-    output cho ra 0x61fe1c 0x61fe1c 0x61fdf0
-    Cau 4:
-    a, Mang duoc truyen vao ham theo co che pass-by-reference
-    b, C++ string duoc truyen vao ham theo co che pass-by-value
-    Cau 5:
-    int main()
-    {
-        int a=4, c=5;
-        int &b=a;
-        cout << &a << " "<< &b << endl;
-        b=c;
-        cout << &c << " " << &b << endl;
-    }
-    a, Bien tham chieu va bien ma no chieu toi la cung mot bien trong bo nho.
-    b, Khong the khai bao mot bien tham chieu ma chua chieu ngay no toi mot bien thuong.
-    c, Khong the chieu mot tham chieu toi mot bien khac voi dich ban dau cua no, tuc nhu o doan code tren, b van chieu toi a sau khi da gan voi c.
-    Cau 6:
-    Khi chay xong ham f, ham f duoc giai phong khoi bo nho va chay den ham g, khi do ham g lai the cho vao bo nho cua ham f (nhat la khi hai ham cung kieu void, tham so
-    giong nhau cung dang int) nen ket qua dua ra 2 dia chi giong nhau.
-
-*/
-bool majority(bool a, bool b, bool c)
+void f(int a[])
 {
-    return a&&b || b&&c || a&&c;
+    cout << sizeof(a) << endl; // in ra dung kich thuoc cua mot bien int* la 8, du kich thuoc cua mang truyen vao co la bao nhieu di nua
 }
-bool eq(int a[], int size_a, int b[], int size_b, int n)
+int count_even(int* a, int n)
 {
-    int cnt=0, i=0, j=0;
-    sort(a, a+size_a); sort(b, b+size_b);
-    while (i<size_a && j <size_b){
-        if (a[i]==b[j]){
-            cnt++;
-            if (cnt==n) return true;
-            i++; j++;
-        }
-        else{
-            cnt=0; i++;
-        }
-    }
-    return false;
-}
-bool checkPrime(int n)
-{
-    for (int i=0; i<=sqrt(n); i++){
-        if (n%i==0)return false;
-    }
-    return n>1;
-}
-void swap(int *a, int *b)
-{
-    int temp=*a;
-    *a=*b;
-    *b=temp;
-}
-int gcd(int a, int b)
-{
-    if (a<b){
-        swap(a,b);
-    }
-    int r=a%b;
-    while (r!=0){
-        a=b;
-        b=r;
-        r=a%b;
-    }
-    return b;
-}
-void printLine(int m, int n)
-{
-    for (int i=0; i<m; i++){
-        cout << " ";
-    }
+    int even=0;
     for (int i=0; i<n; i++){
-        cout << "*";
+        if (a[i]%2==0) even++;
     }
-    cout << endl;
+    return even;
 }
-void printTriangle(int lines)
+bool binarySearch(int *a, int l, int r, int x)
 {
-    for (int i=0; i<lines; i++){
-        printLine(lines-i-1, 2*i+1);
-    }
+    if (l>r) return false;
+    int m=l+(r-l)/2;
+    if (a[m]==x) return true;
+    if (a[m]>x) binarySearch(a, l, r-m-1, x);
+    if (a[m]<x) binarySearch(a, l+m+1, r, x);
 }
-int function_rnd(double x)
-{
-    int res;
-    if (x+0.5 < ceil(x))
-    {
-        res=floor(x);
-    }
-    else{
-        res=ceil(x);
-    }
-    return res;
-}
-int basic_rnd(double x)
-{
-    int temp=(int)x;
-    if (x-(double)temp >= 0.5) return temp+1;
-    return temp;
-}
-int lessThan(int n)
-{
-    return rand()%n;
-}
-void threeDivide25(int a[], int n)
-{
-    for (int i=0; i<n; i++){
-        for (int j=i+1; j<n; j++){
-            for (int k=j+1; k<n; k++){
-                if ((a[i]+a[j]+a[k])%25==0) cout << a[i] << " " << a[j] << " " << a[k] << endl;
-            }
-        }
-    }
-}
-string tenToTwo(int n)
-{
-    string binary="";
-    while (n!=0){
-        if (n%2==0) binary+='0';
-        else binary+='1';
-        n/=2;
-    }
-    reverse(binary.begin(), binary.end());
-    return binary;
-}
-int twoToTen(string binary)
-{
-    int decimal=0;
-    int len=binary.length();
-    reverse(binary.begin(), binary.end());
-    for (int i=0; i<len; i++){
-        if (binary[i]=='1') decimal+=pow(2,i);
-    }
-    return decimal;
-}
-int factorial(int n)
-{
-    int res=1;
-    for (int i=1; i<=n; i++){
-        res*=i;
-    }
-    return res;
-}
-int toHop(int k, int n)
-{
-    return factorial(n)/(factorial(k)*(factorial(n-k)));
-}
-int kiemtra(int k, int n)
-{
-    if (k>=0 && k<=n && n>=1 && n<=20) return 1;
-    return 0;
-}
-//void nhapKN(int k[], int n[], int *soPhanTu)
-//{
-//    *soPhanTu=0;
-//    while (true){
-//        cin >> k[*soPhanTu] >> n[*soPhanTu];
-//        if (k[*soPhanTu]==-1 || n[*soPhanTu]==-1) break;
-//        soPhanTu++;
-//    }
+char* weird_string() {
+   char c[] = "12345";
+   return c;
+}// khong in ra gi
+//int main(int argc, const char * argv[]) {
+//	cout << "Number of command-line arguments: " << argc << endl;
+//	for (int i = 0; i < argc; i++) {
+//		cout << "Argument #" << i << ": _" << argv[i] << "_\n";
+//	}
+//	return 0;
 //}
+int demSoLanXuatHien(const char* a, const char* b)
+{
+    int cnt=0;
+    int lenA=strlen(a);
+    int lenB=strlen(b);
+    for (int i=0; i<lenB; i++){
+        char temp1[lenA], temp2[lenA];//em khong hieu vi sao co bien temp2 thi code cho ra ket qua dung
+        for (int j=0; j<lenA; j++){
+            temp1[j]=b[j+i];
+        }
+        if (strcmp(temp1,a)==0){
+            cnt++;
+        }
+    }
+    return cnt;
+}
 int main()
 {
-    srand(time(NULL));
-    int n; cin >> n;
-    int a[n];
-    for (int i=0; i<n; i++){
-        a[i]=rand()%50;
-    }
-    threeDivide25(a,n);
+    char a[100], b[1000]; cin >> a >> b;
+    cout << demSoLanXuatHien(a,b) << endl;
 }
